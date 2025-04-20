a23263fbf31592c504a5c79f555ac8438bd7bb8b
Hemanth S <gowda.hemanth.1718@gmail.com>
2025-04-20 13:02:14 +0530

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -1,51 +1,51 @@
 // data.ts
 export interface TeamMember {
-  id: number;
-  name: string;
-  image: string;
-  roles: string[]; // Changed role to roles (array of strings)
-  linkedin: string;
-  instagram: string;
-  email: string;
-}
-
-const teamData: TeamMember[] = [
-  {
-    id: 1,
-    name: "Hemanth",
-    image: "/images/Hemanth.jpeg",
-    roles: ["Lead Web Developer", "Machine Learning Lead"], // Added two roles
-    linkedin: "",
-    instagram: "",
-    email: "",
-  },
-  {
-    id: 2,
-    name: "Maralingeshwar",
-    image: "/images/Maralingeshwar.jpeg",
-    roles: ["Web Designer", "ML Innovation Specialist"], // Added two roles
-    linkedin: "",
-    instagram: "",
-    email: "",
-  },
-  {
-    id: 3,
-    name: "Rajath",
-    image: "/images/Rajath.jpeg",
-    roles: ["Product Designer", "ML Team Member"], // Added two roles
-    linkedin: "",
-    instagram: "",
-    email: "",
-  },
-  {
-    id: 4,
-    name: "Mohammad Kaif",
-    image: "/images/Kaif.jpeg",
-    roles: ["Website Team Member", "Product Designer"], // Added two roles
-    linkedin: "",
-    instagram: "",
-    email: "",
-  },
-];
-
-export default teamData;
+    id: number;
+    name: string;
+    image: string;
+    roles: string[]; // Changed role to roles (array of strings)
+    linkedin: string;
+    instagram: string;
+    email: string;
+  }
+  
+  const teamData: TeamMember[] = [
+    {
+      id: 1,
+      name: "Hemanth",
+      image: "/images/Hemanth.jpeg",
+      roles: ["Lead Web Developer", "Machine Learning Lead"], 
+      linkedin: "",
+      instagram: "",
+      email: "",
+    },
+    {
+      id: 2,
+      name: "Maralingeshwar",
+      image: "/images/Maralingeshwar.jpeg",
+      roles: ["Web Designer", "ML Innovation Specialist"], 
+      linkedin: "",
+      instagram: "",
+      email: "",
+    },
+    {
+      id: 3,
+      name: "Rajath",
+      image: "/images/Rajath.jpeg",
+      roles: ["Web Designer", " Lead ML Developer"], 
+      linkedin: "https://www.linkedin.com/in/rajath-u",
+      instagram: "https://www.instagram.com/rajathu_17?igsh=MXgzZTBmdmYwdGxwbQ==",
+      email: "rajath17@yahoo.com",
+    },
+    {
+      id: 4,
+      name: "Mohammad Kaif",
+      image: "/images/Kaif.jpeg",
+      roles: ["Website Team Member", "Product Designer"],
+      linkedin: "https://www.linkedin.com/in/mohammad-kaif-7aab0b228?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      instagram: "",
+      email: "kaif78618@gmail.com",
+    },
+  ];
+  
+  export default teamData;
