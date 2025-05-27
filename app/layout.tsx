32d9351cedf7ba04ddf4eee3dc4ce1a02a580801
Hemanth S <gowda.hemanth.1718@gmail.com>
2025-05-27 18:02:51 +0530

    Enclosed with Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           </header>
           <main  className="bg-[#FAFAFC] w-full">
             {children} {/* Removed container mx-auto px-4 py-8 */}
+            <Toaster />
           </main>
           <footer>
             <Footer />
