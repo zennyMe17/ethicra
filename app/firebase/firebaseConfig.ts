d7a10ecd93ab000a3a8fe462cb36edb63dcc48bc
Hemanth S <gowda.hemanth.1718@gmail.com>
2025-06-08 22:49:22 +0530

    Deployment Changes

--- app/firebase/firebaseConfig.ts
+++ app/firebase/firebaseConfig.ts
@@ -5,26 +5,26 @@ import { getAuth } from "firebase/auth"; // Import getAuth
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 import { FirebaseOptions } from "firebase/app";
 
-// Your Firebase config details
-// const firebaseConfig: FirebaseOptions = {
-//   apiKey: "AIzaSyCQZfnDyWs5I5vuOageLyH4Lsh0bzj6eYs",
-//   authDomain: "fir-auth-cca62.firebaseapp.com",
-//   projectId: "fir-auth-cca62",
-//   storageBucket: "fir-auth-cca62.firebasestorage.app",
-//   messagingSenderId: "578289855665",
-//   appId: "1:578289855665:web:630300dd824cda66b5c690",
-//   measurementId: "G-CFP9T88FJ6",
-// };
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+Your Firebase config details
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyCQZfnDyWs5I5vuOageLyH4Lsh0bzj6eYs",
+  authDomain: "fir-auth-cca62.firebaseapp.com",
+  projectId: "fir-auth-cca62",
+  storageBucket: "fir-auth-cca62.firebasestorage.app",
+  messagingSenderId: "578289855665",
+  appId: "1:578289855665:web:630300dd824cda66b5c690",
+  measurementId: "G-CFP9T88FJ6",
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
